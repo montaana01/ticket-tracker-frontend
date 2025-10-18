f238b909751e28ca4c207bbd9187b8d48ec43806
Alexey Yakauleu iMac Home <yakovlev@yakovlevdev.com>
2025-10-18 20:56:09 +0300

    refactor: update default vite react project

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import {createRoot} from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+
+const rootElement = document.createElement('div');
+rootElement.id = 'root';
+document.body.appendChild(rootElement);
+
+createRoot(rootElement).render(<App/>);
