3a36bfc1d3bf97628ed15fbb8009c92e823d63b9
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-10-28 22:16:16 +0300

    feat: implement theme switcher component

---
+++ src/components/ui/ThemeSwitcher/index.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import { getTheme } from '../../../helpers/getTheme.ts';
+import styles from './theme.module.scss';
+
+const THEME_KEY = 'theme';
+
+export const ThemeSwitcher = () => {
+  const [theme, setTheme] = useState(getTheme(THEME_KEY));
+
+  useEffect(() => {
+    document.body.classList.remove('light', 'dark');
+    document.body.classList.add(theme);
+    localStorage.setItem(THEME_KEY, theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === 'dark' ? 'light' : 'dark'));
+  };
+
+  return (
+    <div className={styles.switcherContainer}>
+      <label id="switcher" className={styles.switcher}>
+        <input
+          id="switcher"
+          type="checkbox"
+          className={styles.switcherInput}
+          checked={theme === 'dark'}
+          onChange={toggleTheme}
+        />
+        <span className={styles.switcherSlider} />
+      </label>
+    </div>
+  );
+};
