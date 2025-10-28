22bd1a06476e571a693c80680174b38379dd13ab
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2025-10-28 23:05:59 +0300

    refactor: update auth buttons component

--- src/components/AuthButtons/index.tsx
+++ src/components/AuthButtons/index.tsx
@@ -1,20 +1,36 @@
-import { useState } from 'react';
 import { NavLink } from 'react-router';
 import { PATHS } from '../../constants/PATHS.ts';
+import styles from './../ui/Header/header.module.scss';
+import { useAuth } from '../../hooks/useAuth.ts';
 
 export const AuthButtons = () => {
-  const [isLoggedIn, setIsLOggedIn] = useState(false);
+  const { role, signOut } = useAuth();
 
   return (
-    <div>
-      {isLoggedIn ? (
-        <button onClick={() => setIsLOggedIn(false)}>Log out</button>
+    <nav className={styles.headerNavigation}>
+      <ul className={styles.headerNavigationWrapper}>
+        {role === 'admin' && (
+          <li className={styles.headerNavigationWrapper}>
+            <NavLink to={PATHS.ADMIN}>Admin Panel</NavLink>
+          </li>
+        )}
+        <li>
+          <NavLink to={PATHS.TICKETS}>Tickets</NavLink>
+        </li>
+        <li>
+          <NavLink to={PATHS.PROFILE}>User info</NavLink>
+        </li>
+      </ul>
+      {role ? (
+        <button onClick={signOut} className={styles.headerButtons + ' btn'}>
+          Log out
+        </button>
       ) : (
-        <>
+        <span className={styles.headerNavigationButtons}>
           <NavLink to={PATHS.SIGN_IN}>Sign In</NavLink>
           <NavLink to={PATHS.SIGN_UP}>Sign Up</NavLink>
-        </>
+        </span>
       )}
-    </div>
+    </nav>
   );
 };
