9188c1453c6c4a336d88cf4d1fc702bb12541481
Alexey Yakauleu iMac Home <yakovlev@yakovlevdev.com>
2025-10-30 01:15:32 +0300

    feat: implement tickets type

---
+++ src/types/tickets.ts
@@ -0,0 +1,25 @@
+export type TagType = {
+  id: number;
+  name: string;
+};
+
+export type StatusType = {
+  id: number;
+  name: string;
+};
+
+export type TicketType = {
+  id: number;
+  title: string;
+  description: string;
+  status_id: number;
+  tag_id: number;
+  message_id: number;
+  author_id: number;
+  updater_id: number;
+  created_at: string;
+  updated_at: string;
+  status_name: string;
+  tag_name: string;
+  author_name: string;
+};
