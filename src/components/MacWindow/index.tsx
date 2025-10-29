939dce69f7ccd618d91eeb0c1f0c2687c829ceff
Alexey Yakauleu iMac Home <yakovlev@yakovlevdev.com>
2025-10-29 21:25:57 +0300

    feat: implement modal view component

--- src/components/MacWindow/index.tsx
+++ src/components/MacWindow/index.tsx
@@ -1,6 +1,42 @@
-import type { ReactNode } from 'react';
+import { type ReactNode, useState } from 'react';
 import styles from './MacWindow.module.scss';
+import container from './../ui/Container/container.module.scss';
 
 export const MacWindow = ({ children }: { children: ReactNode }) => {
-  return <div className={styles.modal}>{children}</div>;
+  const [isMinimized, setIsMinimized] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleClose = () => {
+    setIsVisible(false);
+  };
+
+  const handleMinimize = () => {
+    setIsMinimized((prev) => !prev);
+  };
+
+  if (!isVisible) return null;
+
+  return (
+    <div className={styles.modal}>
+      <div
+        className={`${container.container} ${styles.modalWrapper} ${
+          isMinimized ? styles.minimize : ''
+        }`}
+      >
+        <div className={styles.modalHeader}>
+          <button
+            className={styles.modalHeaderClose + ' ' + styles.modalHeaderButton}
+            onClick={handleClose}
+          ></button>
+          <button
+            className={
+              styles.modalHeaderMinimize + ' ' + styles.modalHeaderButton
+            }
+            onClick={handleMinimize}
+          ></button>
+        </div>
+        <div className={styles.modalContent}>{children}</div>
+      </div>
+    </div>
+  );
 };
