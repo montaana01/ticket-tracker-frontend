340ef4780eb41f74100c4b41d319e5daff05f49f
Alexey Yakauleu iMac Home <yakovlev@yakovlevdev.com>
2025-10-31 00:26:03 +0300

    feat: implement form for creating tickets

---
+++ src/components/ui/TicketForm/index.tsx
@@ -0,0 +1,100 @@
+import { useState } from 'react';
+import {
+  Box,
+  TextField,
+  Button,
+  Typography,
+  CircularProgress,
+  Alert,
+} from '@mui/material';
+import { fetchRequest } from '../../../helpers/fetchRequest.ts';
+import type { CreateTicketFormType } from '../../../types/tickets.ts';
+import { TagSelector } from '../TagSelector';
+
+export const CreateTicketForm = ({
+  onCancel,
+  onSuccess,
+}: CreateTicketFormType) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [tagId, setTagId] = useState<number | undefined>(undefined);
+  const [submitting, setSubmitting] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const submit = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    setSubmitting(true);
+    try {
+      await fetchRequest('/api/tickets', {
+        method: 'POST',
+        body: JSON.stringify({
+          title: title,
+          description: description,
+          tag_id: tagId,
+        }),
+      });
+      onSuccess();
+    } catch {
+      setError(true);
+      setSubmitting(false);
+      setIsLoading(false);
+    }
+  };
+
+  if (isLoading) {
+    return <CircularProgress color="secondary" />;
+  }
+
+  if (error)
+    return (
+      <Alert severity="error" variant="outlined">
+        Error while creating ticket. Please try again later.
+      </Alert>
+    );
+
+  return (
+    <Box component="form" onSubmit={submit}>
+      <Typography variant="h2" typography="h5" gutterBottom>
+        Create ticket
+      </Typography>
+      <TextField
+        label="Title"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+        required
+        sx={{ mb: 2, width: '85%' }}
+      />
+      <TextField
+        label="Description"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+        multiline
+        rows={6}
+        required
+        sx={{ mb: 2, width: '85%' }}
+      />
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'flex-end',
+          width: '85%',
+          margin: '0 auto',
+          mt: 2,
+        }}
+      >
+        <Box sx={{ display: 'flex', gap: 2 }}>
+          <Button variant="contained" type="submit" disabled={submitting}>
+            Create
+          </Button>
+          <Button variant="outlined" onClick={onCancel}>
+            Cancel
+          </Button>
+        </Box>
+        <TagSelector value={tagId} onChange={(id) => setTagId(id)} />
+      </Box>
+    </Box>
+  );
+};
